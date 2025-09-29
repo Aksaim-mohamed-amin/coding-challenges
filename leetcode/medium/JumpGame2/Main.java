4f8e3e737b1814ef221ed3e2e78b6e963e5f21cb
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2025-09-29 21:48:31 +0100

    Jump game 2

---
+++ leetcode/medium/JumpGame2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int[] nums = {2,3,1,1,4};
+		System.out.println(jump(nums));
+	}
+
+	public static int jump(int[] nums) {
+		return 0;
+	}
+}
